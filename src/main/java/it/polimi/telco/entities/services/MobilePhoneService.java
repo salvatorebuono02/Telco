fac3ae8fd99d687bac52d350345c027025f6165b
salvatorebuono02 <salvatore.buono@mail.polimi.it>
2022-06-28 11:18:49 +0200

    initial commit of entities

---
+++ src/main/java/it/polimi/telco/entities/services/MobilePhoneService.java
@@ -0,0 +1,10 @@
+package it.polimi.telco.entities.services;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mobilephoneservice",schema = "telco")
+public class MobilePhoneService extends Service {
+
+}
